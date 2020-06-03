6210049fd5b419b8fbb6b5aa6c5572dca4b0b3ce
andy <evil@kitten>
2020-06-02 21:52:11 -0700

    comments and task

--- 5296bc77afba8baa690002d7.py
+++ 5296bc77afba8baa690002d7.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python
+# This is a solution for a codewars.com kata:
+#Write a function that will solve a 9x9 Sudoku puzzle. The function will take one argument consisting of the 2D puzzle array, with the value 0 representing an unknown square.
+#
+#The Sudokus tested against your function will be "easy" (i.e. determinable; there will be no need to assume and test possibilities on unknowns) and can be solved with a brute-force approach.
+
 def row(puzzle,N): ##return list of all digits in the row N
     return puzzle[N]
 
